233529f1b1b935dad3d14f1c66ba1ce3e9dbc530
Balint Molnar <balintmolnar91@gmail.com>
2019-01-17 17:18:10 +0100

    Add basic kafka statefulset and headless service

--- pkg/apis/banzaicloud/v1alpha1/register.go
+++ pkg/apis/banzaicloud/v1alpha1/register.go
@@ -16,13 +16,13 @@ limitations under the License.
 
 // NOTE: Boilerplate only.  Ignore this file.
 
-// Package v1beta1 contains API Schema definitions for the banzaicloud v1beta1 API group
+// Package v1alpha1 contains API Schema definitions for the banzaicloud v1alpha1 API group
 // +k8s:openapi-gen=true
 // +k8s:deepcopy-gen=package,register
 // +k8s:conversion-gen=github.com/banzaicloud/kafka-operator/pkg/apis/banzaicloud
 // +k8s:defaulter-gen=TypeMeta
 // +groupName=banzaicloud.banzaicloud.io
-package v1beta1
+package v1alpha1
 
 import (
 	"k8s.io/apimachinery/pkg/runtime/schema"
@@ -31,7 +31,7 @@ import (
 
 var (
 	// SchemeGroupVersion is group version used to register these objects
-	SchemeGroupVersion = schema.GroupVersion{Group: "banzaicloud.banzaicloud.io", Version: "v1beta1"}
+	SchemeGroupVersion = schema.GroupVersion{Group: "banzaicloud.banzaicloud.io", Version: "v1alpha1"}
 
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
 	SchemeBuilder = &scheme.Builder{GroupVersion: SchemeGroupVersion}
