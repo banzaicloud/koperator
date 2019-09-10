79d59d731e40965ea1355773f98b3c46ad800d81
Balint Molnar <balintmolnar91@gmail.com>
2019-09-10 17:18:22 +0200

    Add unit test for config generator method

--- pkg/resources/kafka/configmap_test.go
+++ pkg/resources/kafka/configmap_test.go
@@ -14,8 +14,141 @@
 
 package kafka
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/banzaicloud/kafka-operator/api/v1alpha1"
+	"github.com/banzaicloud/kafka-operator/pkg/resources"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
 
 func TestGenerateBrokerConfig(t *testing.T) {
+	tests := []struct {
+		testName                string
+		readOnlyConfig          string
+		clusterWideConfig       string
+		perBrokerReadOnlyConfig string
+		perBrokerConfig         string
+		expectedConfig          string
+	}{
+		{
+			testName:                "basicConfig",
+			readOnlyConfig:          ``,
+			clusterWideConfig:       ``,
+			perBrokerConfig:         ``,
+			perBrokerReadOnlyConfig: ``,
+			expectedConfig: `advertised.listeners=PLAINTEXT://kafka-0.kafka.svc.cluster.local:9092
+broker.id=0
+cruise.control.metrics.reporter.bootstrap.servers=PLAINTEXT://kafka-0.kafka.svc.cluster.local:9092
+listener.security.protocol.map=PLAINTEXT:PLAINTEXT
+listeners=PLAINTEXT://:9092
+metric.reporters=com.linkedin.kafka.cruisecontrol.metricsreporter.CruiseControlMetricsReporter
+security.inter.broker.protocol=PLAINTEXT
+super.users=
+zookeeper.connect=example.zk:2181`,
+		},
+		{
+			testName: "readOnlyRedefinedInOneBroker",
+			readOnlyConfig: `
+auto.create.topics.enable=false
+control.plane.listener.name=thisisatest
+`,
+			clusterWideConfig: `
+background.threads=20
+compression.type=snappy
+`,
+			perBrokerConfig: ``,
+			perBrokerReadOnlyConfig: `
+auto.create.topics.enable=true
+`,
+			expectedConfig: `advertised.listeners=PLAINTEXT://kafka-0.kafka.svc.cluster.local:9092
+auto.create.topics.enable=true
+background.threads=20
+broker.id=0
+compression.type=snappy
+control.plane.listener.name=thisisatest
+cruise.control.metrics.reporter.bootstrap.servers=PLAINTEXT://kafka-0.kafka.svc.cluster.local:9092
+listener.security.protocol.map=PLAINTEXT:PLAINTEXT
+listeners=PLAINTEXT://:9092
+metric.reporters=com.linkedin.kafka.cruisecontrol.metricsreporter.CruiseControlMetricsReporter
+security.inter.broker.protocol=PLAINTEXT
+super.users=
+zookeeper.connect=example.zk:2181`,
+		},
+		{
+			testName: "readOnlyRedefinedInOneBroker",
+			readOnlyConfig: `
+auto.create.topics.enable=false
+control.plane.listener.name=thisisatest
+`,
+			clusterWideConfig: `
+background.threads=20
+compression.type=snappy
+`,
+			perBrokerConfig: `compression.type=default`,
+			perBrokerReadOnlyConfig: `
+auto.create.topics.enable=true
+`,
+			expectedConfig: `advertised.listeners=PLAINTEXT://kafka-0.kafka.svc.cluster.local:9092
+auto.create.topics.enable=true
+background.threads=20
+broker.id=0
+compression.type=default
+control.plane.listener.name=thisisatest
+cruise.control.metrics.reporter.bootstrap.servers=PLAINTEXT://kafka-0.kafka.svc.cluster.local:9092
+listener.security.protocol.map=PLAINTEXT:PLAINTEXT
+listeners=PLAINTEXT://:9092
+metric.reporters=com.linkedin.kafka.cruisecontrol.metricsreporter.CruiseControlMetricsReporter
+security.inter.broker.protocol=PLAINTEXT
+super.users=
+zookeeper.connect=example.zk:2181`,
+		},
+	}
 
+	t.Parallel()
+
+	for _, test := range tests {
+		test := test
+
+		t.Run(test.testName, func(t *testing.T) {
+			r := Reconciler{
+				resources.Reconciler{
+					KafkaCluster: &v1alpha1.KafkaCluster{
+						ObjectMeta: metav1.ObjectMeta{
+							Name:      "kafka",
+							Namespace: "kafka",
+						},
+						Spec: v1alpha1.KafkaClusterSpec{
+							ZKAddresses: []string{"example.zk:2181"},
+							ListenersConfig: v1alpha1.ListenersConfig{
+								InternalListeners: []v1alpha1.InternalListenerConfig{
+									{
+										Type:                            "plaintext",
+										Name:                            "plaintext",
+										UsedForInnerBrokerCommunication: true,
+										ContainerPort:                   9092,
+									},
+								},
+							},
+							ReadOnlyConfig:    test.readOnlyConfig,
+							ClusterWideConfig: test.clusterWideConfig,
+							Brokers: []v1alpha1.Broker{{
+								Id: 0,
+								BrokerConfig: &v1alpha1.BrokerConfig{
+									ReadOnlyConfig: test.perBrokerReadOnlyConfig,
+									Config:         test.perBrokerConfig,
+								},
+							},
+							},
+						},
+					},
+				},
+			}
+			generatedConfig := r.generateBrokerConfig(0, r.KafkaCluster.Spec.Brokers[0].BrokerConfig, []string{}, "", nil)
+
+			if generatedConfig != test.expectedConfig {
+				t.Errorf("the expected config is %s, received: %s", test.expectedConfig, generatedConfig)
+			}
+		})
+	}
 }
