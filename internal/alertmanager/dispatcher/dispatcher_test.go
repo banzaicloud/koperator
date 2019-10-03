fa8796e7eab66a63648da6c57da0be9a962df3f9
Peter Balogh <p.balogh.sa@gmail.com>
2019-10-03 11:52:40 +0200

    implement alert filter

---
+++ internal/alertmanager/dispatcher/dispatcher_test.go
@@ -0,0 +1,83 @@
+// Copyright © 2019 Banzai Cloud
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package dispatcher
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/prometheus/common/model"
+)
+
+func Test_alertFilter(t *testing.T) {
+
+	testAlerts := []model.Alert{
+		model.Alert{
+			Labels: model.LabelSet{
+				"kafka_cr":  "kafka",
+				"namespace": "kafka",
+			},
+			Annotations: model.LabelSet{
+				"command": "testcommand",
+			},
+		},
+		model.Alert{
+			Labels: model.LabelSet{
+				"kafka_cr":  "kafka",
+				"namespace": "kafka",
+			},
+		},
+		model.Alert{
+			Labels: model.LabelSet{
+				"fake":      "fake",
+				"namespace": "kafka",
+			},
+			Annotations: model.LabelSet{
+				"command": "testcommand",
+			},
+		},
+	}
+
+	filteredAlerts := []model.Alert{
+		model.Alert{
+			Labels: model.LabelSet{
+				"kafka_cr":  "kafka",
+				"namespace": "kafka",
+			},
+			Annotations: model.LabelSet{
+				"command": "testcommand",
+			},
+		},
+	}
+
+	tests := []struct {
+		name     string
+		test     []model.Alert
+		filtered []model.Alert
+	}{
+		{
+			"alertFilter",
+			testAlerts,
+			filteredAlerts,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := alertFilter(tt.test); !reflect.DeepEqual(got, tt.filtered) {
+				t.Errorf("alertFilter() = %v, want %v", got, tt.filtered)
+			}
+		})
+	}
+}
