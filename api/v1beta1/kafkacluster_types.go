70b55d5bf7c4d6ee3011a599ba5d90d468a82f7c
Balint Molnar <balintmolnar91@gmail.com>
2020-03-17 11:13:09 +0100

    Use upsctream defined kafka broker jmx_exporter configs

--- api/v1beta1/kafkacluster_types.go
+++ api/v1beta1/kafkacluster_types.go
@@ -519,96 +519,110 @@ func (mConfig *MonitoringConfig) GetKafkaJMXExporterConfig() string {
 	if mConfig.KafkaJMXExporterConfig != "" {
 		return mConfig.KafkaJMXExporterConfig
 	}
+	// Use upstream defined rules https://github.com/prometheus/jmx_exporter/blob/master/example_configs/kafka-2_0_0.yml
 	return `
     lowercaseOutputName: true
-    rules:
-    - pattern : kafka.cluster<type=(.+), name=(.+), topic=(.+), partition=(.+)><>Value
-      name: kafka_cluster_$1_$2
-      labels:
-        topic: "$3"
-        partition: "$4"
-    - pattern : kafka.log<type=Log, name=(.+), topic=(.+), partition=(.+)><>Value
-      name: kafka_log_$1
-      labels:
-        topic: "$2"
-        partition: "$3"
-    - pattern : kafka.controller<type=(.+), name=(.+)><>(Count|Value)
-      name: kafka_controller_$1_$2
-    - pattern : kafka.network<type=(.+), name=(.+)><>Value
-      name: kafka_network_$1_$2
-    - pattern : kafka.network<type=(.+), name=(.+)PerSec, request=(.+)><>Count
-      name: kafka_network_$1_$2_total
-      labels:
-        request: "$3"
-    - pattern : kafka.network<type=(.+), name=(\w+), networkProcessor=(.+)><>Count
-      name: kafka_network_$1_$2
-      labels:
-        request: "$3"
-      type: COUNTER
-    - pattern : kafka.network<type=(.+), name=(\w+), request=(\w+)><>Count
-      name: kafka_network_$1_$2
-      labels:
-        request: "$3"
-      type: COUNTER
-    - pattern : kafka.network<type=(.+), name=(\w+)><>Count
-      name: kafka_network_$1_$2
-      type: COUNTER
-    - pattern : kafka.server<type=(.+), name=(.+)PerSec\w*, topic=(.+)><>Count
-      name: kafka_server_$1_$2_total
-      labels:
-        topic: "$3"
-      type: COUNTER
-    - pattern : kafka.server<type=(.+), name=(.+)PerSec\w*><>Count
-      name: kafka_server_$1_$2_total
-      type: COUNTER
-    - pattern : kafka.server<type=(.+), name=(.+), clientId=(.+), topic=(.+), partition=(.*)><>(Count|Value)
-      name: kafka_server_$1_$2
-      labels:
-        clientId: "$3"
-        topic: "$4"
-        partition: "$5"
-    - pattern : kafka.server<type=(.+), name=(.+), topic=(.+), partition=(.*)><>(Count|Value)
-      name: kafka_server_$1_$2
-      labels:
-        topic: "$3"
-        partition: "$4"
-    - pattern : kafka.server<type=(.+), name=(.+), topic=(.+)><>(Count|Value)
-      name: kafka_server_$1_$2
-      labels:
-        topic: "$3"
-      type: COUNTER
-    - pattern : kafka.server<type=(.+), name=(.+), clientId=(.+), brokerHost=(.+), brokerPort=(.+)><>(Count|Value)
-      name: kafka_server_$1_$2
-      labels:
-        clientId: "$3"
-        broker: "$4:$5"
-    - pattern : kafka.server<type=(.+), name=(.+), clientId=(.+)><>(Count|Value)
-      name: kafka_server_$1_$2
-      labels:
-        clientId: "$3"
-    - pattern : kafka.server<type=(.+), name=(.+)><>(Count|Value)
-      name: kafka_server_$1_$2
-    - pattern : kafka.(\w+)<type=(.+), name=(.+)PerSec\w*><>Count
-      name: kafka_$1_$2_$3_total
-      type: COUNTER
-    - pattern : kafka.(\w+)<type=(.+), name=(.+)PerSec\w*, topic=(.+)><>Count
-      name: kafka_$1_$2_$3_total
-      labels:
-        topic: "$4"
-      type: COUNTER
-    - pattern : kafka.(\w+)<type=(.+), name=(.+)PerSec\w*, topic=(.+), partition=(.+)><>Count
-      name: kafka_$1_$2_$3_total
-      labels:
-        topic: "$4"
-        partition: "$5"
-      type: COUNTER
-    - pattern : kafka.(\w+)<type=(.+), name=(.+)><>(Count|Value)
-      name: kafka_$1_$2_$3_$4
-      type: COUNTER
-    - pattern : kafka.(\w+)<type=(.+), name=(.+), (\w+)=(.+)><>(Count|Value)
-      name: kafka_$1_$2_$3_$6
-      labels:
-        "$4": "$5"
+	rules:
+	# Special cases and very specific rules
+	- pattern : kafka.server<type=(.+), name=(.+), clientId=(.+), topic=(.+), partition=(.*)><>Value
+	  name: kafka_server_$1_$2
+	  type: GAUGE
+	  labels:
+		clientId: "$3"
+		topic: "$4"
+		partition: "$5"
+	- pattern : kafka.server<type=(.+), name=(.+), clientId=(.+), brokerHost=(.+), brokerPort=(.+)><>Value
+	  name: kafka_server_$1_$2
+	  type: GAUGE
+	  labels:
+		clientId: "$3"
+		broker: "$4:$5"
+	- pattern : kafka.coordinator.(\w+)<type=(.+), name=(.+)><>Value
+	  name: kafka_coordinator_$1_$2_$3
+	  type: GAUGE
+
+	# Generic per-second counters with 0-2 key/value pairs
+	- pattern: kafka.(\w+)<type=(.+), name=(.+)PerSec\w*, (.+)=(.+), (.+)=(.+)><>Count
+	  name: kafka_$1_$2_$3_total
+	  type: COUNTER
+	  labels:
+		"$4": "$5"
+		"$6": "$7"
+	- pattern: kafka.(\w+)<type=(.+), name=(.+)PerSec\w*, (.+)=(.+)><>Count
+	  name: kafka_$1_$2_$3_total
+	  type: COUNTER
+	  labels:
+		"$4": "$5"
+	- pattern: kafka.(\w+)<type=(.+), name=(.+)PerSec\w*><>Count
+	  name: kafka_$1_$2_$3_total
+	  type: COUNTER
+
+	- pattern: kafka.server<type=(.+), client-id=(.+)><>([a-z-]+)
+	  name: kafka_server_quota_$3
+	  type: GAUGE
+	  labels:
+		resource: "$1"
+		clientId: "$2"
+
+	- pattern: kafka.server<type=(.+), user=(.+), client-id=(.+)><>([a-z-]+)
+	  name: kafka_server_quota_$4
+	  type: GAUGE
+	  labels:
+		resource: "$1"
+		user: "$2"
+		clientId: "$3"
+
+	# Generic gauges with 0-2 key/value pairs
+	- pattern: kafka.(\w+)<type=(.+), name=(.+), (.+)=(.+), (.+)=(.+)><>Value
+	  name: kafka_$1_$2_$3
+	  type: GAUGE
+	  labels:
+		"$4": "$5"
+		"$6": "$7"
+	- pattern: kafka.(\w+)<type=(.+), name=(.+), (.+)=(.+)><>Value
+	  name: kafka_$1_$2_$3
+	  type: GAUGE
+	  labels:
+		"$4": "$5"
+	- pattern: kafka.(\w+)<type=(.+), name=(.+)><>Value
+	  name: kafka_$1_$2_$3
+	  type: GAUGE
+
+	# Emulate Prometheus 'Summary' metrics for the exported 'Histogram's.
+	#
+	# Note that these are missing the '_sum' metric!
+	- pattern: kafka.(\w+)<type=(.+), name=(.+), (.+)=(.+), (.+)=(.+)><>Count
+	  name: kafka_$1_$2_$3_count
+	  type: COUNTER
+	  labels:
+		"$4": "$5"
+		"$6": "$7"
+	- pattern: kafka.(\w+)<type=(.+), name=(.+), (.+)=(.*), (.+)=(.+)><>(\d+)thPercentile
+	  name: kafka_$1_$2_$3
+	  type: GAUGE
+	  labels:
+		"$4": "$5"
+		"$6": "$7"
+		quantile: "0.$8"
+	- pattern: kafka.(\w+)<type=(.+), name=(.+), (.+)=(.+)><>Count
+	  name: kafka_$1_$2_$3_count
+	  type: COUNTER
+	  labels:
+		"$4": "$5"
+	- pattern: kafka.(\w+)<type=(.+), name=(.+), (.+)=(.*)><>(\d+)thPercentile
+	  name: kafka_$1_$2_$3
+	  type: GAUGE
+	  labels:
+		"$4": "$5"
+		quantile: "0.$6"
+	- pattern: kafka.(\w+)<type=(.+), name=(.+)><>Count
+	  name: kafka_$1_$2_$3_count
+	  type: COUNTER
+	- pattern: kafka.(\w+)<type=(.+), name=(.+)><>(\d+)thPercentile
+	  name: kafka_$1_$2_$3
+	  type: GAUGE
+	  labels:
+		quantile: "0.$4"
 `
 }
 
