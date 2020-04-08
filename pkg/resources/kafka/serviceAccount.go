ccd19f2f47a1d291c3f592867a0d761d2fd99d6a
Balazs Berta <balazs.berta.dev@gmail.com>
2020-04-08 20:31:20 +0200

    Refactor serviceaccount reconcile, fix rbac

--- pkg/resources/kafka/serviceAccount.go
+++ pkg/resources/kafka/serviceAccount.go
@@ -15,65 +15,31 @@
 package kafka
 
 import (
-	"context"
 	"fmt"
 
 	"github.com/banzaicloud/kafka-operator/api/v1beta1"
 	"github.com/banzaicloud/kafka-operator/pkg/util"
 	corev1 "k8s.io/api/core/v1"
-	apiErrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/types"
 )
 
-func (r *Reconciler) serviceAccount() (runtime.Object, error) {
-
-	exists, err := r.serviceAccountForKafkaClusterExists()
-	if err != nil {
-		return nil, err
-	}
-
-	if !exists {
-		serviceAccount := &corev1.ServiceAccount{
-			ObjectMeta: metav1.ObjectMeta{
-				Namespace:   r.KafkaCluster.Namespace,
-				Name:        fmt.Sprintf(v1beta1.ServiceAccountNameFormat, r.KafkaCluster.Name),
-				Labels:      LabelsForKafka(r.KafkaCluster.Name),
-				Annotations: nil,
-				OwnerReferences: []metav1.OwnerReference{
-					{
-						APIVersion:         r.KafkaCluster.APIVersion,
-						Kind:               r.KafkaCluster.Kind,
-						Name:               r.KafkaCluster.Name,
-						UID:                r.KafkaCluster.UID,
-						Controller:         util.BoolPointer(true),
-						BlockOwnerDeletion: util.BoolPointer(true),
-					},
+func (r *Reconciler) serviceAccount() runtime.Object {
+	return &corev1.ServiceAccount{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: r.KafkaCluster.Namespace,
+			Name:      fmt.Sprintf(v1beta1.ServiceAccountNameFormat, r.KafkaCluster.Name),
+			Labels:    LabelsForKafka(r.KafkaCluster.Name),
+			OwnerReferences: []metav1.OwnerReference{
+				{
+					APIVersion:         r.KafkaCluster.APIVersion,
+					Kind:               r.KafkaCluster.Kind,
+					Name:               r.KafkaCluster.Name,
+					UID:                r.KafkaCluster.UID,
+					Controller:         util.BoolPointer(true),
+					BlockOwnerDeletion: util.BoolPointer(true),
 				},
 			},
-		}
-
-		return serviceAccount, nil
+		},
 	}
-
-	return nil, nil
-}
-
-func (r *Reconciler) serviceAccountForKafkaClusterExists() (bool, error) {
-	serviceAccount := corev1.ServiceAccount{}
-	namespacedName := types.NamespacedName{
-		Namespace: r.KafkaCluster.Namespace,
-		Name:      fmt.Sprintf(v1beta1.ServiceAccountNameFormat, r.KafkaCluster.Name),
-	}
-
-	err := r.Get(context.Background(), namespacedName, &serviceAccount)
-	if err != nil {
-		if apiErrors.IsNotFound(err) {
-			return false, nil
-		}
-		return false, err
-	}
-
-	return true, nil
 }
