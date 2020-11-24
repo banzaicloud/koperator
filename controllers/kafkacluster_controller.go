d8d28fb6dc0b05a1a4c1645de048abc027ac7f80
adamantal <adam.antal@banzaicloud.com>
2020-11-24 17:01:35 +0100

    Adding validation and post-update checks to the dynamic per-broker config setting

--- controllers/kafkacluster_controller.go
+++ controllers/kafkacluster_controller.go
@@ -160,10 +160,10 @@ func (r *KafkaClusterReconciler) Reconcile(request ctrl.Request) (ctrl.Result, e
 				return ctrl.Result{
 					RequeueAfter: time.Duration(20) * time.Second,
 				}, nil
-			case errorfactory.PerBrokerConfigUpdated:
+			case errorfactory.PerBrokerConfigNotReady:
 				log.V(1).Info("broker configuration was dynamically updated")
 				return ctrl.Result{
-					RequeueAfter: time.Duration(3) * time.Second,
+					RequeueAfter: time.Duration(15) * time.Second,
 				}, nil
 			default:
 				return requeueWithError(log, err.Error(), err)
