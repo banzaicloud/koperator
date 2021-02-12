baa7083327fa517efeb496d8b9b2bfa4b9314452
adamantal <aadam99@gmail.com>
2021-02-12 14:43:05 +0100

    Covering user provided input, and supporting non JBOD case

--- pkg/resources/cruisecontrol/configmap_test.go
+++ pkg/resources/cruisecontrol/configmap_test.go
@@ -26,7 +26,7 @@ import (
 	"github.com/banzaicloud/kafka-operator/api/v1beta1"
 )
 
-func TestGenerateCapacityConfig(t *testing.T) {
+func TestGenerateCapacityConfig_JBOD(t *testing.T) {
 	quantity, _ := resource.ParseQuantity("10Gi")
 	cpuQuantity, _ := resource.ParseQuantity("2000m")
 
@@ -400,6 +400,342 @@ func TestGenerateCapacityConfig(t *testing.T) {
 			}
 
 			var expected CapacityConfig
+			err = json.Unmarshal([]byte(test.expectedConfiguration), &expected)
+			if err != nil {
+				t.Error(err, "could not unmarshal expected json")
+			}
+
+			if !reflect.DeepEqual(actual, expected) {
+				t.Error("Expected:", expected, ", got:", actual)
+			}
+		})
+	}
+}
+
+func TestGenerateCapacityConfigWithUserProvidedInput(t *testing.T) {
+	cpuQuantity, _ := resource.ParseQuantity("2000m")
+
+	testCases := []struct {
+		testName              string
+		capacityConfig        string
+		expectedConfiguration string
+	}{
+		{
+			testName: "JBOD case, without default broker",
+			capacityConfig: `
+                  {
+				  "brokerCapacities":[
+					{
+					  "brokerId": "0",
+					  "capacity": {
+						"DISK": {"/tmp/kafka-logs": "500000"},
+						"CPU": "100",
+						"NW_IN": "50000",
+						"NW_OUT": "50000"
+					  },
+					  "doc": "This overrides the capacity for broker 0. This broker is not a JBOD broker."
+					},
+					{
+					  "brokerId": "1",
+					  "capacity": {
+						"DISK": {"/tmp/kafka-logs-1": "250000", "/tmp/kafka-logs-2": "250000"},
+						"CPU": "100",
+						"NW_IN": "50000",
+						"NW_OUT": "50000"
+					  },
+					  "doc": "This overrides the capacity for broker 1. This broker is a JBOD broker."
+					}
+				  ]
+				}`,
+			expectedConfiguration: `
+                  {
+				  "brokerCapacities":[
+					{
+					  "brokerId": "0",
+					  "capacity": {
+						"DISK": {"/tmp/kafka-logs": "500000"},
+						"CPU": "100",
+						"NW_IN": "50000",
+						"NW_OUT": "50000"
+					  },
+					  "doc": "This overrides the capacity for broker 0. This broker is not a JBOD broker."
+					},
+					{
+					  "brokerId": "1",
+					  "capacity": {
+						"DISK": {"/tmp/kafka-logs-1": "250000", "/tmp/kafka-logs-2": "250000"},
+						"CPU": "100",
+						"NW_IN": "50000",
+						"NW_OUT": "50000"
+					  },
+					  "doc": "This overrides the capacity for broker 1. This broker is a JBOD broker."
+					},
+					{
+					  "brokerId": "-1",
+					  "capacity": {
+						"DISK": {"/kafka-logs/kafka": "10737418240"},
+						"CPU": "100",
+						"NW_IN": "125000",
+						"NW_OUT": "125000"
+					  },
+					  "doc": "Capacity unit used for disk is in MB, cpu is in percentage, network throughput is in KB."
+					}
+				  ]
+				}`,
+		},
+		{
+			testName: "JBOD case, with default broker",
+			capacityConfig: `
+                  {
+				  "brokerCapacities":[
+					{
+					  "brokerId": "-1",
+					  "capacity": {
+						"DISK": {"/tmp/kafka-logs-1": "100000", "/tmp/kafka-logs-2": "100000", "/tmp/kafka-logs-3": "50000",
+						  "/tmp/kafka-logs-4": "50000", "/tmp/kafka-logs-5": "150000", "/tmp/kafka-logs-6": "50000"},
+						"CPU": "100",
+						"NW_IN": "10000",
+						"NW_OUT": "10000"
+					  },
+					  "doc": "The default capacity for a broker with multiple logDirs each on a separate heterogeneous disk."
+					},
+					{
+					  "brokerId": "0",
+					  "capacity": {
+						"DISK": {"/tmp/kafka-logs": "500000"},
+						"CPU": "100",
+						"NW_IN": "50000",
+						"NW_OUT": "50000"
+					  },
+					  "doc": "This overrides the capacity for broker 0. This broker is not a JBOD broker."
+					},
+					{
+					  "brokerId": "1",
+					  "capacity": {
+						"DISK": {"/tmp/kafka-logs-1": "250000", "/tmp/kafka-logs-2": "250000"},
+						"CPU": "100",
+						"NW_IN": "50000",
+						"NW_OUT": "50000"
+					  },
+					  "doc": "This overrides the capacity for broker 1. This broker is a JBOD broker."
+					}
+				  ]
+				}`,
+			expectedConfiguration: `
+                  {
+				  "brokerCapacities":[
+					{
+					  "brokerId": "-1",
+					  "capacity": {
+						"DISK": {"/tmp/kafka-logs-1": "100000", "/tmp/kafka-logs-2": "100000", "/tmp/kafka-logs-3": "50000",
+						  "/tmp/kafka-logs-4": "50000", "/tmp/kafka-logs-5": "150000", "/tmp/kafka-logs-6": "50000"},
+						"CPU": "100",
+						"NW_IN": "10000",
+						"NW_OUT": "10000"
+					  },
+					  "doc": "The default capacity for a broker with multiple logDirs each on a separate heterogeneous disk."
+					},
+					{
+					  "brokerId": "0",
+					  "capacity": {
+						"DISK": {"/tmp/kafka-logs": "500000"},
+						"CPU": "100",
+						"NW_IN": "50000",
+						"NW_OUT": "50000"
+					  },
+					  "doc": "This overrides the capacity for broker 0. This broker is not a JBOD broker."
+					},
+					{
+					  "brokerId": "1",
+					  "capacity": {
+						"DISK": {"/tmp/kafka-logs-1": "250000", "/tmp/kafka-logs-2": "250000"},
+						"CPU": "100",
+						"NW_IN": "50000",
+						"NW_OUT": "50000"
+					  },
+					  "doc": "This overrides the capacity for broker 1. This broker is a JBOD broker."
+					}
+				  ]
+				}`,
+		},
+		{
+			testName: "without JBOD and default broker",
+			capacityConfig: `
+                  {
+				  "brokerCapacities":[
+					{
+					  "brokerId": "0",
+					  "capacity": {
+						"DISK": "500000",
+						"CPU": "100",
+						"NW_IN": "50000",
+						"NW_OUT": "50000"
+					  },
+					  "doc": "This overrides the capacity for broker 0. This broker is not a JBOD broker."
+					},
+					{
+					  "brokerId": "1",
+					  "capacity": {
+						"DISK": "250000",
+						"CPU": "100",
+						"NW_IN": "50000",
+						"NW_OUT": "50000"
+					  },
+					  "doc": "This overrides the capacity for broker 1. This broker is a JBOD broker."
+					}
+				  ]
+				}`,
+			expectedConfiguration: `
+                  {
+				  "brokerCapacities":[
+					{
+					  "brokerId": "0",
+					  "capacity": {
+						"DISK": "500000",
+						"CPU": "100",
+						"NW_IN": "50000",
+						"NW_OUT": "50000"
+					  },
+					  "doc": "This overrides the capacity for broker 0. This broker is not a JBOD broker."
+					},
+					{
+					  "brokerId": "1",
+					  "capacity": {
+						"DISK": "250000",
+						"CPU": "100",
+						"NW_IN": "50000",
+						"NW_OUT": "50000"
+					  },
+					  "doc": "This overrides the capacity for broker 1. This broker is a JBOD broker."
+					},
+					{
+					  "brokerId": "-1",
+					  "capacity": {
+						"DISK": {"/kafka-logs/kafka": "10737418240"},
+						"CPU": "100",
+						"NW_IN": "125000",
+						"NW_OUT": "125000"
+					  },
+					  "doc": "Capacity unit used for disk is in MB, cpu is in percentage, network throughput is in KB."
+					}
+				  ]
+				}`,
+		},
+		{
+			testName: "without JBOD case, but with default broker",
+			capacityConfig: `
+                  {
+				  "brokerCapacities":[
+					{
+					  "brokerId": "-1",
+					  "capacity": {
+						"DISK": "100000",
+						"CPU": "100",
+						"NW_IN": "10000",
+						"NW_OUT": "10000"
+					  },
+					  "doc": "The default capacity for a broker with multiple logDirs each on a separate heterogeneous disk."
+					},
+					{
+					  "brokerId": "0",
+					  "capacity": {
+						"DISK": "500000",
+						"CPU": "100",
+						"NW_IN": "50000",
+						"NW_OUT": "50000"
+					  },
+					  "doc": "This overrides the capacity for broker 0. This broker is not a JBOD broker."
+					},
+					{
+					  "brokerId": "1",
+					  "capacity": {
+						"DISK": "250000",
+						"CPU": "100",
+						"NW_IN": "50000",
+						"NW_OUT": "50000"
+					  },
+					  "doc": "This overrides the capacity for broker 1. This broker is a JBOD broker."
+					}
+				  ]
+				}`,
+			expectedConfiguration: `
+                  {
+				  "brokerCapacities":[
+					{
+					  "brokerId": "-1",
+					  "capacity": {
+						"DISK": "100000",
+						"CPU": "100",
+						"NW_IN": "10000",
+						"NW_OUT": "10000"
+					  },
+					  "doc": "The default capacity for a broker with multiple logDirs each on a separate heterogeneous disk."
+					},
+					{
+					  "brokerId": "0",
+					  "capacity": {
+						"DISK": "500000",
+						"CPU": "100",
+						"NW_IN": "50000",
+						"NW_OUT": "50000"
+					  },
+					  "doc": "This overrides the capacity for broker 0. This broker is not a JBOD broker."
+					},
+					{
+					  "brokerId": "1",
+					  "capacity": {
+						"DISK": "250000",
+						"CPU": "100",
+						"NW_IN": "50000",
+						"NW_OUT": "50000"
+					  },
+					  "doc": "This overrides the capacity for broker 1. This broker is a JBOD broker."
+					}
+				  ]
+				}`,
+		},
+	}
+
+	t.Parallel()
+
+	for _, test := range testCases {
+		test := test
+
+		t.Run(test.testName, func(t *testing.T) {
+			kafkaCluster := v1beta1.KafkaCluster{
+				Spec: v1beta1.KafkaClusterSpec{
+					Brokers: []v1beta1.Broker{
+						{
+							Id: 0,
+							BrokerConfig: &v1beta1.BrokerConfig{
+								Resources: &v1.ResourceRequirements{
+									Limits: v1.ResourceList{
+										"cpu": cpuQuantity,
+									}},
+							},
+						},
+						{
+							Id: 1,
+						},
+						{
+							Id: 2,
+						},
+						{
+							Id: 4,
+						},
+					},
+					CruiseControlConfig: v1beta1.CruiseControlConfig{
+						CapacityConfig: test.capacityConfig,
+					},
+				},
+			}
+			var actual JBODInvariantCapacityConfig
+			err := json.Unmarshal([]byte(GenerateCapacityConfig(&kafkaCluster, log.NullLogger{}, nil)), &actual)
+			if err != nil {
+				t.Error(err, "could not actual unmarshal json")
+			}
+
+			var expected JBODInvariantCapacityConfig
 			err = json.Unmarshal([]byte(test.expectedConfiguration), &expected)
 			if err != nil {
 				t.Error(err, "could not unmarshal expected json")
