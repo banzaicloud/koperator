76ec4cea7598f2305d79cbbf9633a0be18ae69ac
Adrian Muraru <amuraru@adobe.com>
2022-11-21 16:57:09 +0200

    Upgrade PodDisruptionBudget API to v1 (#888)
    Co-authored-by: Patrik Egyed <8093632+pregnor@users.noreply.github.com>

--- controllers/tests/kafkacluster_controller_kafka_test.go
+++ controllers/tests/kafkacluster_controller_kafka_test.go
@@ -22,7 +22,7 @@ import (
 	. "github.com/onsi/gomega"
 
 	corev1 "k8s.io/api/core/v1"
-	policyv1beta1 "k8s.io/api/policy/v1beta1"
+	policyv1 "k8s.io/api/policy/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/intstr"
@@ -107,7 +107,7 @@ func expectKafkaPDB(kafkaCluster *v1beta1.KafkaCluster) {
 	waitForClusterRunningState(kafkaCluster, kafkaCluster.Namespace)
 
 	// get created PDB
-	pdb := policyv1beta1.PodDisruptionBudget{}
+	pdb := policyv1.PodDisruptionBudget{}
 	Eventually(func() error {
 		return k8sClient.Get(context.Background(), types.NamespacedName{
 			Namespace: kafkaCluster.Namespace,
--- pkg/resources/kafka/poddisruptionbudget.go
+++ pkg/resources/kafka/poddisruptionbudget.go
@@ -28,7 +28,7 @@ import (
 	"github.com/banzaicloud/koperator/pkg/resources/templates"
 	"github.com/banzaicloud/koperator/pkg/util"
 
-	policyv1beta1 "k8s.io/api/policy/v1beta1"
+	policyv1 "k8s.io/api/policy/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -39,10 +39,10 @@ func (r *Reconciler) podDisruptionBudget(log logr.Logger) (runtime.Object, error
 		return nil, err
 	}
 
-	return &policyv1beta1.PodDisruptionBudget{
+	return &policyv1.PodDisruptionBudget{
 		TypeMeta: metav1.TypeMeta{
 			Kind:       "PodDisruptionBudget",
-			APIVersion: "policy/v1beta1",
+			APIVersion: "policy/v1",
 		},
 		ObjectMeta: templates.ObjectMetaWithAnnotations(
 			fmt.Sprintf("%s-pdb", r.KafkaCluster.Name),
@@ -50,7 +50,7 @@ func (r *Reconciler) podDisruptionBudget(log logr.Logger) (runtime.Object, error
 			r.KafkaCluster.Spec.ListenersConfig.GetServiceAnnotations(),
 			r.KafkaCluster,
 		),
-		Spec: policyv1beta1.PodDisruptionBudgetSpec{
+		Spec: policyv1.PodDisruptionBudgetSpec{
 			MinAvailable: &minAvailable,
 			Selector: &metav1.LabelSelector{
 				MatchLabels: apiutil.LabelsForKafka(r.KafkaCluster.Name),
