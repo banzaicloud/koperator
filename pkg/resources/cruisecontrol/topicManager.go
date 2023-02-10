8c76627bf116abbcfbe9fd392ab1e8cbe386cf5b
mihaialexandrescu <40660914+mihaialexandrescu@users.noreply.github.com>
2023-02-10 15:29:18 +0200

    Rename webhooks errors exported funcs (#925)
    * pkg webhooks: update new error functions names to IsAdmission prefix; NOT API breaking
    
    * pkg webhooks: update IsInvalidReplicationFactor exported func name with IsAdmission prefix; API breaking change

--- pkg/resources/cruisecontrol/topicManager.go
+++ pkg/resources/cruisecontrol/topicManager.go
@@ -99,7 +99,7 @@ func generateCCTopic(cluster *v1beta1.KafkaCluster, client client.Client, log lo
 					return errorfactory.New(errorfactory.ResourceNotReady{}, err, "topic admission failed to connect to kafka cluster")
 				}
 				// If less than the required brokers are available - return not ready
-				if webhooks.IsInvalidReplicationFactor(err) {
+				if webhooks.IsAdmissionInvalidReplicationFactor(err) {
 					return errorfactory.New(errorfactory.ResourceNotReady{}, err, fmt.Sprintf("not enough brokers available (at least %d needed) for CC topic", topic.Spec.ReplicationFactor))
 				}
 				return errorfactory.New(errorfactory.APIFailure{}, err, "could not create cruise control topic")
